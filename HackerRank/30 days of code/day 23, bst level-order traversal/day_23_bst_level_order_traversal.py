96e3b0a5c565ece9da182a7bf41cf485a70b53f6
Fernando Zuher <fernando.zuher@gmail.com>
2022-03-16 20:27:20 -0300

    day 23c

--- HackerRank/30 days of code/day 23, bst level-order traversal/day_23_bst_level_order_traversal.py
+++ HackerRank/30 days of code/day 23, bst level-order traversal/day_23_bst_level_order_traversal.py
@@ -1,51 +1,55 @@
 import sys
 
+
 class Node:
-    def __init__(self,data):
-        self.right=self.left=None
+    def __init__(self, data):
+        self.right = self.left = None
         self.data = data
+
+
 class Solution:
-    def insert(self,root,data):
-        if root==None:
+    def insert(self, root, data):
+        if root == None:
             return Node(data)
         else:
-            if data<=root.data:
-                cur=self.insert(root.left,data)
-                root.left=cur
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
             else:
-                cur=self.insert(root.right,data)
-                root.right=cur
+                cur = self.insert(root.right, data)
+                root.right = cur
         return root
 
     # Immutable HackerRank code above
     def levelOrder(self, root):
-        if not root:
-            return;
 
         from collections import deque
 
-        myQueue = Queue
-        myQueue.push(root);
-        process.stdout.write(myQueue[0].data + " ");
+        if not root:
+            return
 
-        for ( ; myQueue.length; myQueue.shift()) {
-            
-            if (myQueue[0].left) {
-                myQueue.push(myQueue[0].left);
-                process.stdout.write(myQueue[0].left.data + " ");
-            }
+        myQueue = deque()
+        myQueue.append(root)
+        print(myQueue[0].data, end=" ")
 
-            if (myQueue[0].right) {
-                myQueue.push(myQueue[0].right);
-                process.stdout.write(myQueue[0].right.data + " ");
-            }
-        }
+        while len(myQueue):
+
+            if myQueue[0].left:
+                myQueue.append(myQueue[0].left)
+                print(myQueue[0].left.data, end=" ")
+
+            if myQueue[0].right:
+                myQueue.append(myQueue[0].right)
+                print(myQueue[0].right.data, end=" ")
+
+            myQueue.popleft()
+
 
 # Immutable HackerRank code below
-T=int(input())
-myTree=Solution()
-root=None
+T = int(input())
+myTree = Solution()
+root = None
 for i in range(T):
-    data=int(input())
-    root=myTree.insert(root,data)
+    data = int(input())
+    root = myTree.insert(root, data)
 myTree.levelOrder(root)
